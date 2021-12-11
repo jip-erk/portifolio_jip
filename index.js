2f9b4852a8f5f58450139b6c61781b97eb6f9c86
jipErkelens <79573058+jipErkelens@users.noreply.github.com>
2021-12-11 15:46:39 +0100

    yes

--- index.js
+++ index.js
@@ -13,5 +13,20 @@ var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
 
 
+"use strict";
 
-
+let dots = 4;
+let sliderElem = document.querySelector('.slider');
+let dotElems = sliderElem.querySelectorAll('.slider__dot');
+let indicatorElem = sliderElem.querySelector('.slider__indicator');
+Array.prototype.forEach.call(dotElems, dotElem => {
+  dotElem.addEventListener('click', e => {
+    let currentPos = parseInt(sliderElem.getAttribute('data-pos'));
+    let newPos = parseInt(dotElem.getAttribute('data-pos'));
+    let newDirection = newPos > currentPos ? 'right' : 'left';
+    let currentDirection = newPos < currentPos ? 'right' : 'left';
+    indicatorElem.classList.remove(`slider__indicator--${currentDirection}`);
+    indicatorElem.classList.add(`slider__indicator--${newDirection}`);
+    sliderElem.setAttribute('data-pos', newPos);
+  });
+});
